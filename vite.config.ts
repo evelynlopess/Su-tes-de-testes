a05d7f79d0e0498283042b28593e5e0941f5e89c
Evelyn Lopes <86618154+evelynlopess@users.noreply.github.com>
2026-01-16 11:44:26 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'evelynlopess/Su-tes-de-testes',
+  base: '/Su-tes-de-testes',
   plugins: [react()],
   server: {
     port: 3000,
